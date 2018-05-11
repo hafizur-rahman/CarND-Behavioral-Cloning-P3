32bf8bc2c22d4ce057a9bedaf3905a2f88565bab
Hafizur Rahman <bibagimon@gmail.com>
2018-05-12 00:16:34 +0900

    Track 1 success

--- drive.py
+++ drive.py
@@ -11,6 +11,7 @@ import eventlet.wsgi
 from PIL import Image
 from flask import Flask
 from io import BytesIO
+import cv2
 
 from keras.models import load_model
 import h5py
@@ -44,7 +45,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 9 
 controller.set_desired(set_speed)
 
 
@@ -57,11 +58,19 @@ def telemetry(sid, data):
         throttle = data["throttle"]
         # The current speed of the car
         speed = data["speed"]
+        
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        
+        cropped = image_array[60:-10,20:-20,:]
+        resized = cv2.resize(cropped,(140, 50), interpolation = cv2.INTER_AREA)
+        
+#         cropped = image_array[60:-10,30:-30]
+#         resized = cv2.resize(cropped,(140, 50), interpolation = cv2.INTER_AREA)
+        
+        steering_angle = float(model.predict(resized[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
